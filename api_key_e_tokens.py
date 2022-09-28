95ccfbb9a4ceb5765e82504c064be0e0d266ae59
maria2122 <mariajsantos199@gmail.com>
2022-09-28 18:13:40 -0300

    Organização de Requests em Funções

--- api_key_e_tokens.py
+++ api_key_e_tokens.py
@@ -1,13 +1,14 @@
-'''
--------------------------------------- API_KEYS e Tokens -----------------------------------------------
-
-Para acessarmos tais variáveis no nosso programa, instalaremos o pacote python-dotenv na versão 0.19.2
-'''
 import os
 from symbol import parameters
 from urllib import response
 import requests
 from dotenv import load_dotenv
+
+'''
+-------------------------------------- API_KEYS e Tokens : Requests With Security-----------------------------------------------
+
+Para acessarmos tais variáveis no nosso programa, instalaremos o pacote python-dotenv na versão 0.19.2
+'''
 
 #1)seta o carregamento das variávies de ambiente e permite que as acessamos
 load_dotenv()
@@ -29,126 +30,125 @@ session.params = {
 #OBS: Para fazermos requisições na sessão que criamos, utilizamos o objeto 'session' ao invés da função requests.get. Por exemplo, uma requisição get seria algo como: session.get('...')
 
 #2) Obtendo meus quadros no Trello
-
+def getBoardTrello():
 #Parâmetros para ser enviado pela url
-parameters  = {
-    #Campos que queremos obter as informações 
-    'fields':['name', 'url','idBoard']
-}
+    parameters  = {
+        #Campos que queremos obter as informações 
+        'fields':['name', 'url','idBoard']
+    }
 
-#Requisição
-response = session.get(f'https://api.trello.com/1/members/me/boards', params=parameters )
+    #Requisição
+    response = session.get(f'https://api.trello.com/1/members/me/boards', params=parameters )
 
-#Verificação do 'response', se ele retoenar com status 200, será exibido os dados
-if response.status_code == 200:
-    #Lista de dicionário contendo resposta
-    data = response.json()
+    #Verificação do 'response', se ele retoenar com status 200, será exibido os dados
+    if response.status_code == 200:
+        #Lista de dicionário contendo resposta
+        data = response.json()
 
-    #Percorre a lista de dicionário imprimindo informações 'name', 'url' e 'id'.
-    for board in data:
-        print(f'ID: {[board["id"]]}')
-        print(f'NAME: {[board["name"]]}')
-        print(f'URL: {[board["url"]]}')
-        print('\n')
-else:
-    print('An error occurred, call an adult.')
+        #Percorre a lista de dicionário imprimindo informações 'name', 'url' e 'id'.
+        for board in data:
+            print(f'ID: {[board["id"]]}')
+            print(f'NAME: {[board["name"]]}')
+            print(f'URL: {[board["url"]]}')
+            print('\n')
+    else:
+        print('An error occurred, call an adult.')
 
 #3) Criando meus quadros no Trello
+def createBoardTrello():
+    #Parâmetros da URL
 
+    parameters = {
+        'name':'Neps API Course',
+        'desc':"That's a test board for the Neps API Couse"
+    }
 
-#Parâmetros da URL
+    #Requisição POST - Cria novo quadro com parâmetros especificados acima
+    response = session.post('https://api.trello.com/1/boards', params=parameters)
 
-parameters = {
-    'name':'Neps API Course',
-    'desc':"That's a test board for the Neps API Couse"
-}
+    #Exibe a resposta se o status for de sucesso, do contrário exibe a mensagem de errro.
+    if response.status_code == 200:
+        # Dados do novo quadro
+        data = response.json()
 
-#Requisição POST - Cria novo quadro com parâmetros especificados acima
-response = session.post('https://api.trello.com/1/boards', params=parameters)
-
-#Exibe a resposta se o status for de sucesso, do contrário exibe a mensagem de errro.
-if response.status_code == 200:
-    # Dados do novo quadro
-    data = response.json()
-
-    print(f'Name: {data["name"]}')
-    print(f'Description: {data["desc"]}')
-    print(f'URL: {data["url"]}')
-    print(f'ID: {data["id"]}')
-else:
-    print('An error occurred, call an adult.')
+        print(f'Name: {data["name"]}')
+        print(f'Description: {data["desc"]}')
+        print(f'URL: {data["url"]}')
+        print(f'ID: {data["id"]}')
+    else:
+        print('An error occurred, call an adult.')
 
 #4) Criando lista no quadro criado
+def createListInBoard():
+    parameters  = {
+        #Campos que queremos obter as informações 
+        'name':'List number 2',
+        'idBoard':'6331945367d85704f403f56a',
+    }
 
-parameters  = {
-    #Campos que queremos obter as informações 
-    'name':'List number 2',
-    'idBoard':'6331945367d85704f403f56a',
-}
+    #Requisição POST
+    response = session.post('https://api.trello.com/1/lists/', params=parameters )
 
-#Requisição POST
-response = session.post('https://api.trello.com/1/lists/', params=parameters )
+    if response.status_code == 200:
+        #Dados da lista criada
+        data = response.json()
 
-if response.status_code == 200:
-    #Dados da lista criada
-    data = response.json()
-
-    #imprimindo informações 'name', 'id' e 'idBoard'.
-    
-    print(f'List name: {data["name"]}')
-    print(f'ID: {data["id"]}')
-    print(f'ID_Quadro: {data["idBoard"]}')
-else:
-    print('An error occurred, call an adult.')
+        #imprimindo informações 'name', 'id' e 'idBoard'.
+        
+        print(f'List name: {data["name"]}')
+        print(f'ID: {data["id"]}')
+        print(f'ID_Quadro: {data["idBoard"]}')
+    else:
+        print('An error occurred, call an adult.')
 
 #5) Adicionando um cartão na lista criada
+def addCardInList():
+    parameters = {
+        'name':'Second Card in To Do List',
+        'desc':"That's a card for testing in List number 2",
+        'idList':'633362cb3f83b602ee5d7da5'
+    }
 
-parameters = {
-    'name':'Second Card in To Do List',
-    'desc':"That's a card for testing in List number 2",
-    'idList':'633362cb3f83b602ee5d7da5'
-}
+    #Requisição POST
+    response = session.post('https://api.trello.com/1/cards',params=parameters)
 
-#Requisição POST
-response = session.post('https://api.trello.com/1/cards',params=parameters)
+    if response.status_code == 200:
+        #Dados do cartão criado
+        data = response.json()
 
-if response.status_code == 200:
-    #Dados do cartão criado
-    data = response.json()
+        print(f'Card name: {data["name"]}')
+        print(f'ID: {data["id"]}')
+        print(f'Description: {data["desc"]}')
+    else:
+        print('An error occurred, call an adult.')
 
-    print(f'Card name: {data["name"]}')
-    print(f'ID: {data["id"]}')
-    print(f'Description: {data["desc"]}')
-else:
-    print('An error occurred, call an adult.')
+#6) Editando um Cartão 
+def editCard():
+    parameters = {
+        'name': 'Edited Card number 2',
+        'desc': "That' card a test to edit"
+    }
 
-#6) Editando um Card 
+    #Requisição POST
+    response = session.put('https://api.trello.com/1/cards/633365e3aaea1103e2169687', params=parameters)
 
-parameters = {
-    'name': 'Edited Card number 2',
-    'desc': "That' card a test to edit"
-}
-
-#Requisição POST
-response = session.put('https://api.trello.com/1/cards/633365e3aaea1103e2169687', params=parameters)
-
-if response.status_code == 200:
-    #Dados do card depois da alteração
-    data = response.json()
-    print(f'Card name: {data["name"]}')
-    print(f'ID: {data["id"]}')
-    print(f'Description: {data["desc"]}')
-else:
-    print('An error occurred, call an adult.')
+    if response.status_code == 200:
+        #Dados do card depois da alteração
+        data = response.json()
+        print(f'Card name: {data["name"]}')
+        print(f'ID: {data["id"]}')
+        print(f'Description: {data["desc"]}')
+    else:
+        print('An error occurred, call an adult.')
 
 #7) Deletando um cartão 
+def deleteCard():
+    parameters = {
+        'idList':'633362cb3f83b602ee5d7da5'
+    }
+    response = session.delete('https://api.trello.com/1/cards/633365e3aaea1103e2169687', params=parameters)
 
-parameters = {
-    'idList':'633362cb3f83b602ee5d7da5'
-}
-response = session.delete('https://api.trello.com/1/cards/633365e3aaea1103e2169687', params=parameters)
-
-if response.status_code == 200:
-    print('Deleted with successful')
-else:
-    print('An error occurred, call an adult.')
+    if response.status_code == 200:
+        print('Deleted with successful')
+    else:
+        print('An error occurred, call an adult.')
--- request.py
+++ request.py
@@ -2,109 +2,123 @@ import json
 from urllib import response
 import requests
 
-# # #1) Com a biblioteca 'REQUESTS' basta chamá-la passando a URL e parâmetros da API(para get é opcional), no caso eu o 'params' especificamos que queremos a resposta em json
-response = requests.get('http://numbersapi.com/22/math', params='json')
+# ------------- Requisições Básicas nas APIS: NUMBERSAPI(traz curiosidades sobre os números) e Jsonplaceholder(permite testes e simulações de requisições) -------------------------------------------------
 
-# #Recebe o status da requisição e compara se esse é 200 de Ok ou 404 de Não encontrada
-status_code = response.status_code
-if(status_code==200):
-    print("Resposta Ok!")
-elif(status_code==404):
-    print("Rota não encontrada :(")
+#NUMBERAPI API
+# #1) Com a biblioteca 'REQUESTS' basta chamá-la passando a URL e parâmetros da API(para get é opcional), no caso eu o 'params' especificamos que queremos a resposta em json
+def curiosityOfNumber22():
+    
+    response = requests.get('http://numbersapi.com/22/math', params='json')
 
+    # #Recebe o status da requisição e compara se esse é 200 de Ok ou 404 de Não encontrada
+    status_code = response.status_code
+    if(status_code==200):
+        print("Resposta Ok!")
+    elif(status_code==404):
+        print("Rota não encontrada :(")
 
-print(f'Text: {response.text}')
-print(f'Status Code: {response.status_code}')
+    #Imprimindo alguns dados da resposta
+    print(f'Text: {response.text}')
+    print(f'Status Code: {response.status_code}')
 
-# ''' Resposta
-# Resposta Ok!
-# Text: {
-#  "text": "22 is an even composite number, its proper divisors being 1, 2 and 11.",
-#  "number": 22,
-#  "found": true,
-#  "type": "math"
-# }
-# '''
+    # Captura a URL depois de feita a requisição
+    print(f'URL: {response.url}')
+    #  Resposta
+    # Resposta Ok!
+    # Text: {
+    #  "text": "22 is an even composite number, its proper divisors being 1, 2 and 11.",
+    #  "number": 22,
+    #  "found": true,
+    #  "type": "math"
+    # }
+    # 
 
-# #2) Captura a URL depois de feita a requisição
-print(f'URL: {response.url}')
+    # #Podemos utilizar a função .json() do python para transformar a resposta da requisição em um dicionário, como no exemplo.
+    
+    json = response.json()
+    print(f'Tipo: {type(json)}')
+    print(f'Dados: {json}')
 
-# #JSON da resposta
-json = response.json()
-print(f'Tipo: {type(json)}')
-print(f'Dados: {json}')
+    # Resposta
+    # URL: http://numbersapi.com/22/math?json
+    # Tipo: <class 'dict'>
+    # Dados: {'text': '22 is the number of partitions of 8.', 'number': 22, 'found': True, 'type': 'math'}
+    # 
 
-# '''Resposta
-# URL: http://numbersapi.com/22/math?json
-# Tipo: <class 'dict'>
-# Dados: {'text': '22 is the number of partitions of 8.', 'number': 22, 'found': True, 'type': 'math'}
-# '''
+# #2) Se quisermos enviar vários parâmetros pela URL basta criar um dicionário e passar ele na requisição - exemplo fictício
+def multipleParametersInURL():
+    parametros = {'key1':'value1','key2':'value2'}
 
-# #3) Se quisermos enviar vários parâmetros pela URL basta criar um dicionário e passar ele na requisição
+    # #Requisição 'apenas teste'
+    response = requests.get('https://www.test.com/', params=parametros)
+    print(response.url)
 
-parametros = {'key1':'value1','key2':'value2'}
+# #3) Lendo um número e passando na requisição.
+def readNumberSendRequest():
+    number = int(input('Entre com um número'))
 
-# #Requisição 'apenas teste'
-response = requests.get('https://www.test.com/', params=parametros)
-print(response.url)
+    # # #Requisição
+    response = requests.get(f'http://numbersapi.com/{number}/math', params='json')
 
-# #4) Lendo um número e passando na requisição.
-number = int(input('Entre com um número'))
-
-# # #Requisição
-response = requests.get(f'http://numbersapi.com/{number}/math', params='json')
-
-# Verifica se o status da resposta é válida
-if(response.status_code == 200):
-    #Dicionário contendo os dados da resposta
-    data = response.json()
-    #Verifica se a requisição foi encontrada
-    if(data['found']==True):
-        print(data['text'])
+    # Verifica se o status da resposta é válida
+    if(response.status_code == 200):
+        #Dicionário contendo os dados da resposta
+        data = response.json()
+        #Verifica se a requisição foi encontrada
+        if(data['found']==True):
+            print(data['text'])
+        else:
+            print('Desculpe, rota nao encontrada.')
     else:
-        print('Desculpe, rota nao encontrada.')
-else:
-    print('URL invalida.')
+        print('URL invalida.')
 
-'''Resposta:
-1 is also the first and second numbers in the Fibonacci sequence and is the first number in many other mathematical sequences.
-'''
-#5) Enviando dados pelo corpo da requisição com POST com a API https://jsonplaceholder.typicode.com/
+    '''Resposta:
+    1 is also the first and second numbers in the Fibonacci sequence and is the first number in many other mathematical sequences.
+    '''
 
-#Dicionário contendo os parâmetros para envio no corpo da requisição(modelo JSON)
-data = {
-    "title": "Esse eh o meu POST",
-    "body": "Esse eh o corpo da requisicao",
-    "userID": 1
-}
+#JSONPLACEHOLDER API
+#4) Enviando dados pelo corpo da requisição com POST com a API https://jsonplaceholder.typicode.com/
+def sendDataBodyRequest():
 
-# # #Requisição
-response = requests.post('https://jsonplaceholder.typicode.com/posts/', json=data)
+    #Dicionário contendo os parâmetros para envio no corpo da requisição(modelo JSON)
+    data = {
+        "title": "Esse eh o meu POST",
+        "body": "Esse eh o corpo da requisicao",
+        "userID": 1
+    }
 
-# #Informações da resposta
-print(f'Status Code: {response.status_code}')
-print(f'Dados: {response.json()}')
+    # # #Requisição
+    response = requests.post('https://jsonplaceholder.typicode.com/posts/', json=data)
 
-'''Resposta:
-Status Code: 201
-Dados: {'title': 'Esse eh o meu POST', 'body': 'Esse eh o corpo da requisicao', 'userID': 1, 'id': 101}
-'''
-# #6) Atualizando recurso (método PUT)
-response = requests.put('https://jsonplaceholder.typicode.com/posts/1', json = data)
-print(f'Status Code: {response.status_code}')
-print(f'Dados: {response.json()}')
+    # #Informações da resposta
+    print(f'Status Code: {response.status_code}')
+    print(f'Dados: {response.json()}')
 
-'''Resposta:
-Status Code: 200
-Dados: {'title': 'Esse eh o meu POST', 'body': 'Esse eh o corpo da requisicao', 'userID': 1, 'id': 1}
-'''
-# #7) Deletando recurso (método DELETE)
-response = requests.delete('https://jsonplaceholder.typicode.com/posts/1')
-print(f'Status Code: {response.status_code}')
-print(f'Dados: {response.json()}')
+    '''Resposta:
+    Status Code: 201
+    Dados: {'title': 'Esse eh o meu POST', 'body': 'Esse eh o corpo da requisicao', 'userID': 1, 'id': 101}
+    '''
 
-'''Resposta:
-Status Code: 200
-Dados: {}
-'''
+# #5) Atualizando recurso (método PUT)
+def updateFeatue():
+
+    response = requests.put('https://jsonplaceholder.typicode.com/posts/1', json = data)
+    print(f'Status Code: {response.status_code}')
+    print(f'Dados: {response.json()}')
+
+    '''Resposta:
+    Status Code: 200
+    Dados: {'title': 'Esse eh o meu POST', 'body': 'Esse eh o corpo da requisicao', 'userID': 1, 'id': 1}
+    '''
+
+# #6) Deletando recurso (método DELETE)
+def deleteFeature():
+    response = requests.delete('https://jsonplaceholder.typicode.com/posts/1')
+    print(f'Status Code: {response.status_code}')
+    print(f'Dados: {response.json()}')
+
+    '''Resposta:
+    Status Code: 200
+    Dados: {}
+    '''
 
